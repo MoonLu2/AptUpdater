91b57ead4ed48eb32b27bdff70779f01b0242d96
MoonyLu2 <153979873+MoonLu2@users.noreply.github.com>
2026-03-21 13:44:26 +0200

    A slight fix...

--- AptUpdater.py
+++ AptUpdater.py
@@ -4,7 +4,6 @@ def welcome():
     print("")
     exitq1 = input('   [APT_UPDATER(CurrentProgram)] Do you want to continue? (y/N)->')
 
-
 # A  l o t  o f  c h e c k s  f o r  t h e  u s e r ' s  p r e f e r e n c e .
 
     if exitq1 == 'Y' or exitq1 == 'y':
@@ -24,6 +23,7 @@ def upgrade():
         print('''Here it's "apt upgrade"''')
         os.system('sudo -S apt upgrade')
 
+
 def end():
     import os
     exitq2 = input('   [APT_UPDATER(CurrentProgram)] Are you sure you need to update? (y/N)->')
@@ -38,16 +38,16 @@ def end():
         return 0
     else:
         print('[APT_UPDATER(CurrentProgram)] syntax-error: input invalid')
-        exit(64)
+        return 64
 
-welcome()
-upgrade()
-end()
-   
+# welcome()
+# upgrade()
+# end()
+
 # The code won't go over if it's imported. (the __main__ == "__name__" module was suggested)
 if __name__ == "__main__":
-    exit(welcome())
-    exit(upgrade())
+    welcome()
+    upgrade()
     exit(end())
 else:
     print(f'''execution-err in {__name__}, this code was not intented for 'import'.''')
